f05339ea271dbd3dea94e6236192f8d9483b14a7
Will Burland <williamburland10@gmail.com>
2023-06-30 20:25:05 +0100

    Initial commit

---
+++ make.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cd build
+del pokemon_sprite.exe
+
+cls
+
+echo Building...
+
+g++ -std=c++17 -O2 ^
+src/main.cpp ^
+-o pokemon_sprite.exe
+
+echo Done
+echo.
+
+if exist pokemon_sprite.exe (
+	pokemon_sprite.exe
+)
